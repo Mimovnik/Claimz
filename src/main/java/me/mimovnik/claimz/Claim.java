21883bdf76d8c821cd9599e03070ceb9697755a7
mimovnik-manjaro <jkwidzinski@gmail.com>
2022-12-14 00:00:50 +0100

    Mark a region and display it boundaries

---
+++ src/main/java/me/mimovnik/claimz/Claim.java
@@ -0,0 +1,61 @@
+package me.mimovnik.claimz;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+
+import static org.bukkit.Particle.ELECTRIC_SPARK;
+
+public class Claim {
+    private int minX, maxX, minY, maxY, minZ, maxZ;
+    private World world;
+
+    public Claim(Location firstCorner, Location secondCorner, World world) {
+        this.world = world;
+        minX = Math.min(firstCorner.getBlockX(), secondCorner.getBlockX());
+        maxX = Math.max(firstCorner.getBlockX(), secondCorner.getBlockX());
+
+        minY = Math.min(firstCorner.getBlockY(), secondCorner.getBlockY());
+        maxY = Math.max(firstCorner.getBlockY(), secondCorner.getBlockY());
+
+        minZ = Math.min(firstCorner.getBlockZ(), secondCorner.getBlockZ());
+        maxZ = Math.max(firstCorner.getBlockZ(), secondCorner.getBlockZ());
+    }
+
+    public void display() {
+        Particle particle = ELECTRIC_SPARK;
+        // X edges
+        for (int x = minX; x < maxX; x++) {
+            world.spawnParticle(particle, x, minY, minZ, 10);
+            world.spawnParticle(particle, x, minY, maxZ, 10);
+            world.spawnParticle(particle, x, maxY, minZ, 10);
+            world.spawnParticle(particle, x, maxY, maxZ, 10);
+        }
+        // Y edges
+        for (int y = minY; y < maxY; y++) {
+            world.spawnParticle(particle, minX, y, minZ, 10);
+            world.spawnParticle(particle, minX, y, maxZ, 10);
+            world.spawnParticle(particle, maxX, y, minZ, 10);
+            world.spawnParticle(particle, maxX, y, maxZ, 10);
+        }
+        // Z edges
+        for (int z = minZ; z < maxZ; z++) {
+            world.spawnParticle(particle, minX, minY, z, 10);
+            world.spawnParticle(particle, minX, maxY, z, 10);
+            world.spawnParticle(particle, maxX, minY, z, 10);
+            world.spawnParticle(particle, maxX, maxY, z, 10);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Claim{" +
+                "minX=" + minX +
+                ", maxX=" + maxX +
+                ", minY=" + minY +
+                ", maxY=" + maxY +
+                ", minZ=" + minZ +
+                ", maxZ=" + maxZ +
+                '}';
+    }
+}
---
+++ src/main/java/me/mimovnik/claimz/ClaimEditor.java
@@ -0,0 +1,86 @@
+package me.mimovnik.claimz;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.*;
+
+public class ClaimEditor implements Listener {
+    private Location firstCorner, secondCorner;
+    private Boolean isHoldingEditorTool;
+    private Material editorTool = Material.STICK;
+    private Claim claim;
+    private ScheduledExecutorService particleRenderer = Executors.newSingleThreadScheduledExecutor();
+
+    public ClaimEditor() {
+        particleRenderer.scheduleAtFixedRate(this::displayClaim, 0, 250, MILLISECONDS);
+    }
+
+    private void displayClaim() {
+        if (claim != null && isHoldingEditorTool) {
+            claim.display();
+        }
+    }
+
+    @EventHandler
+    public void onPlayerItemHeld(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItem(event.getNewSlot());
+        if (item == null) {
+            player.sendMessage("You are NOT holding the editor tool.");
+            isHoldingEditorTool = false;
+            return;
+        }
+
+        if (item.getType().equals(editorTool)) {
+            player.sendMessage("You are holding the editor tool.");
+            isHoldingEditorTool = true;
+
+
+        } else {
+            player.sendMessage("You are NOT holding the editor tool.");
+            isHoldingEditorTool = false;
+        }
+    }
+
+    @EventHandler
+    public void onRightClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (isHoldingEditorTool && event.getHand() == EquipmentSlot.HAND) {
+            if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                Block block = event.getClickedBlock();
+                if (block == null) {
+                    player.sendMessage("Claim setup cancelled");
+                    firstCorner = null;
+                    secondCorner = null;
+                } else {
+                    if (firstCorner == null) {
+                        firstCorner = block.getLocation();
+                        player.sendMessage("First Corner set to:" + block.getLocation());
+                    } else {
+                        secondCorner = block.getLocation();
+                        player.sendMessage("Second Corner set to:" + block.getLocation());
+                        claim = new Claim(firstCorner, secondCorner, player.getWorld());
+                        player.sendMessage("Claim set to" + claim);
+                        firstCorner = null;
+                        secondCorner = null;
+                    }
+                }
+            }
+        }
+    }
+
+}
