2d901fe4ce8bf34dd935080463d50a7cb1b5aded
mimovnik-manjaro <jkwidzinski@gmail.com>
2022-12-14 13:23:01 +0100

    Disallow other players to place blocks in your claim

--- src/main/java/me/mimovnik/claimz/ClaimGuard.java
+++ src/main/java/me/mimovnik/claimz/ClaimGuard.java
@@ -2,11 +2,12 @@ package me.mimovnik.claimz;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockMultiPlaceEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 import java.util.ArrayList;
 
@@ -17,16 +18,27 @@ public class ClaimGuard implements Listener {
         this.claims = claims;
     }
 
-    @EventHandler
-    public void onBreakBlock(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        Location location = event.getBlock().getLocation();
-
+    private boolean cancelEvent(Player player, Location location){
         for (Claim claim : claims) {
             if (claim.contains(location) && player != claim.getOwner()) {
-                event.setCancelled(true);
-                player.sendMessage("You can't break a block here. It's " + ChatColor.ITALIC + claim.getOwner().getName() + ChatColor.RESET + "'s land.");
+                player.sendMessage(ChatColor.RED + "You don't have permission to do that. It's " + ChatColor.ITALIC + "" + ChatColor.YELLOW + claim.getOwner().getName() + ChatColor.RED + "'s claim.");
+                return true;
             }
         }
+        return false;
+    }
+    @EventHandler
+    public void onBreakBlock(BlockBreakEvent event) {
+        event.setCancelled(cancelEvent(event.getPlayer(), event.getBlock().getLocation()));
+    }
+
+    @EventHandler
+    public void onPlaceBlock(BlockPlaceEvent event){
+        event.setCancelled(cancelEvent(event.getPlayer(), event.getBlock().getLocation()));
+    }
+
+    @EventHandler
+    public void onMultiPlaceBlock(BlockMultiPlaceEvent event){
+        event.setCancelled(cancelEvent(event.getPlayer(), event.getBlock().getLocation()));
     }
 }
